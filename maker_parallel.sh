f71aaa6fc7ef3b95c38d9ed487e92353a7dc9b6b
Michael Li <mc2li@uwaterloo.ca>
2013-07-16 14:32:49 +0000

    2183: Change parallel environment to smp. -ML
    git-svn-id: http://svn.biodiversity.agr.gc.ca/repo/source/Maker@3666 8f470391-0214-0410-be57-dbd57b47fce5

--- maker_parallel.sh
+++ maker_parallel.sh
@@ -186,5 +186,5 @@ do
 	echo $i >> index
 done
 
-qsub -N "Maker_Parallel" -cwd -q all.q -p $priority -b y -v new_dir=$new_dir,filename=$filename,cpu=$cpu -V -pe orte $cpu -t 1-$num_contig:1 $script_path/$(basename $0)
+qsub -N "Maker_Parallel" -cwd -q all.q -p $priority -b y -v new_dir=$new_dir,filename=$filename,cpu=$cpu -V -pe smp $cpu -t 1-$num_contig:1 $script_path/$(basename $0)
 
