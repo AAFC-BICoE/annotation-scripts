febdea062bee49347c43b7eb6eae95650c39515e
Michael Li <mc2li@uwaterloo.ca>
2013-07-24 20:13:33 +0000

    2233: Improved file storage and offloaded all of the io to each inidivual task. -ML
    git-svn-id: http://svn.biodiversity.agr.gc.ca/repo/source/Maker@3732 8f470391-0214-0410-be57-dbd57b47fce5

--- maker_parallel.sh
+++ maker_parallel.sh
@@ -3,27 +3,27 @@
 
 # This script will parallelize maker by splitting the given fasta into individual contigs 
 # This script will take advantage of SGE's task arrays
-# Usage: maker_parallel.sh <file input> <working directory> '<ID field separator>' [priority] 
+# Usage: maker_parallel.sh -f <file input> -w <working directory> -d <ID field separator> -p [priority]
+# -c [cpu]
 # 
 # Parameters:
-# <file input> 
+# -f <file input> 
 #	is simply the genome/multi-fasta file you want to work with. This script will copy
 # 	The fasta file into the working directory, if it is not already there. This protects
 # 	source data from any clobber. The path to the file can be relative.
 #
-# <working directory> 
+# -w <working directory> 
 #	The folder where all the output will be found. In addition to the working directory,
 # 	there will be two additional folders inside. The script will make a data folder to 
 #	hold the analysis of each contig and a log folder to contain all of qsubs output. 
 #	This leaves the working directory to contain the source fasta file, the folders,
 #	Maker's configuration files, and a datastore index.
 #
-# <ID field separator> 
-#	A single quoted string that separates information in the fasta file. Each sequence
-#	in a fasta file has an indentification line that starts with a ">". This line contains 
-#	the name and additional information. The information is usually separated by some 
-#	character. However, the delimiting character isn't standard, so this needs to be given
-#	to the script. PLEASE note that this should be in single quotes. (e.g. ' ')
+# -d <ID field separator> 
+#	Each sequence in a fasta file has an indentification line that starts with a ">".
+#	This line contains the name and additional information. The information is usually 
+#	separated by some character. However, the delimiting character isn't standard, 
+# 	so this needs to be given to the script.
 #
 # Optional Arguments:
 # Priority
@@ -46,28 +46,45 @@ qsub_maker () {
 
 	cd $new_dir #Just makes sure we're in the correct directory
 	
-	# Bash does not allow arrays to be exported (due to safety)
-	# We must retrieve our file name from a index file (See Line 118)
-	file_array=( $(cat logs/index) )
+	# Pulls the nth sequence from the fasta file.
+	awk -v RS='>' -v seq=$SGE_TASK_ID 'NR>=(seq+1)&&NR<=(seq+1){print ">"$0}'  $new_dir/$(basename $from) > contig_$SGE_TASK_ID.fasta
 
-	# If data exists as a file, we throw a warning to prevent clobber
-	if [[ -e data && (! -d data) ]]; then
-		echo "There is a file named data in the working directory. Please move it to resume and restart"
-		exit 0;
+	# Pulls the name from the fasta file
+	contig_name=`head -n 1 contig_$SGE_TASK_ID.fasta | grep "^>"| cut $delimiter -f 1 | cut -d'>' -f 2`
+	# This decides which folder structure to use. If there's less than 300 contigs, it should be fine
+	# to leave it all in one folder. Otherwise, we'll group into 50 folders.
+	# We will use folder_struct to hold the folder names after data.
+	if [ $SGE_TASK_LAST -ge 300 ]; then
+		# Determines how many contigs in each folder
+		per_folders=$(( $SGE_TASK_LAST / 50 ))
+		
+		# Determines which folder this contig will go into
+		folder=$(( $SGE_TASK_ID / $per_folders ))
+
+		# This is just to have the last folder matching the number of the last contig.
+		if [[ ( $(( $SGE_TASK_ID % 50 )) -ne 0 ) && ( $folder -eq 50 ) ]]; then
+			folder_struct="$((per_folders * 50))-$SGE_TASK_LAST/$contig_name"
+		else
+			min_num=$(($folder * $per_folders))
+			max_num=$(( ($folder+1) * $per_folders - 1 ))
+			folder_struct="$min_num-$max_num/$contig_name"
+		fi
+	else
+		folder_struct=$contig_name
 	fi
-
-	mkdir -p data
+	
+	# Creates the folders, if not already there.
+	mkdir -p data/$folder_struct
+	# Get the fasta file to the new folder
+	mv contig_$SGE_TASK_ID.fasta data/$folder_struct/$contig_name.fasta
 	
 	cd data
-	mkdir -p ${file_array[$num]} #This will allow us to make each individual folder
-
-	# Populates/Gathers files to begin maker
-	mv $new_dir/${file_array[$num]}.fasta ${file_array[$num]}/
-	cp ../maker_{opts,bopts,exe}.ctl ${file_array[$num]}/
-	cd ${file_array[$num]}
-
+	# Gathers Maker config files
+	cp ../maker_{opts,bopts,exe}.ctl $folder_struct/
+	
+	cd $folder_struct
 	# Adjusts the genome to point to the new file.
-	sed -i s%^genome=.*%genome=$new_dir/data/${file_array[$num]}/${file_array[$num]}.fasta% maker_opts.ctl
+	sed -i s%^genome=.*%genome=$new_dir/data/$folder_struct/$contig_name.fasta% maker_opts.ctl
 	# Adjusts cpu usage as requested by user
 	sed -i s%^cpus=.*%cpus=$cpu% maker_opts.ctl
 	
@@ -80,19 +97,16 @@ qsub_maker () {
 	while read line
 	do
 		name="$(echo $line | cut -d ' ' -f 1)"
-		echo $name
-		loca="$new_dir/data/${file_array[$num]}/${file_array[$num]}.maker.output/$(echo $line | cut -d ' ' -f 2)"
-		echo $loca
+		loca="$new_dir/data/$folder_struct/$contig_name.maker.output/$(echo $line | cut -d ' ' -f 2)"
 		stat="$(echo $line | cut -d ' ' -f 3)"
-		echo $stat
 		printf "$name\t$loca\t$stat\n" >> index.tmp
-	done < ${file_array[$num]}.maker.output/${file_array[$num]}_master_datastore_index.log
+	done < $contig_name.maker.output/$contig_name_master_datastore_index.log
 
 	cat index.tmp >> $new_dir/$filename\_master_datastore_index.log
 	rm index.tmp
 }
 
-# MAIN FUNCTION BEGINE
+# MAIN FUNCTION BEGIN
 
 # Checks if we are in qsub, if so begin running maker
 if [[ ! -z $SGE_TASK_ID ]]; then
@@ -103,54 +117,63 @@ fi
 #A safe way to catch errors early. Ends the script if a command fails.
 set -e
 
-# Outputs the standard help/usage info
-if [[ -z $1 || -z $2 ]]; then
-        echo "Usage: $0 <fasta_file> <working_directory> '<identifier line delimiter>' [priority] [cpus]"
-        echo "This script will split a given fasta file into n parts. Each file will be in their own folder. The maker configuration files must be in the working directory."
-	echo "Note the single quotes around the delimiter!"
-        exit 0;
+usage="Usage: $0 -f <fasta_file> -w <working_directory> -d <identifier line delimiter> -p [priority] -c [cpus] \n
+This script will split a given fasta file into n parts. Each file will be in their own folder. The maker configuration files must be in the working directory."
+
+while getopts :f:w:dpc: optname
+do
+	case "$optname"	in
+		f) file=${OPTARG};;
+		w) new_dir=${OPTARG};;
+		d) delimiter="-d \"${OPTARG}\"";;
+		p) priority=${OPTARG};;
+		c) cpu=${OPTARG};;
+		*) echo -e $usage; exit 0;;
+		?) echo -e $usage; exit 0;;
+	esac
+done
+
+if [ $(( $# % 2)) -ne 0 ]; then
+	echo -e $usage
+	exit 0;
 fi
 
 # Checks for valid file
-if [[ ! ($1 == *.fa*) || ($1 == *.genome*) ]]; then
+if [[ ! ($file == *.fa*) || ($file == *.genome*) ]]; then
 	echo "Unrecognized fasta file. File must have .fa* or .genome ending."
 	exit 0;
 fi
 
 # Checks if you provided maker's configuration file.
-if [[ ! (-a $2/maker_bopts.ctl && -a $2/maker_exe.ctl && -a $2/maker_opts.ctl) ]]; then
+if [[ -z $new_dir || ! (-e $new_dir/maker_bopts.ctl && -e $new_dir/maker_exe.ctl && -e $new_dir/maker_opts.ctl) ]]; then
 	echo "Cannot find maker configuration files. You can generate blank configuration files by using maker's -CTL flag."
 	exit 0;
 fi
 
 # Assigns a default priority value
-if [[ -z $4 ]]; then
+if [[ -z $priority ]]; then
 	priority=0
-else
-	priority=$4
 fi
 
 # Assigns a default priority value
-if [[ -z $5 ]]; then
+if [[ -z $cpu ]]; then
         cpu=1
-else
-        cpu=$5
 fi
 
 printf "Creating initial variables... "
 
 # Creates a copy of the genome in the working directory if it's not already there
 # This is to prevent clobber and write permission issues
-if [[ ! -e $2/$(basename $1) ]]; then
-	cp $1 $2/
+if [[ ! -e $new_dir/$(basename $file) ]]; then
+	cp $file $new_dir/
 fi
 
 # A two step process to just get the basename of a file w/o extension
-filename=$(basename "$1")
+filename=$(basename "$file")
 filename="${filename%.*}"
 
 # Aquires absolute paths of script and working directory
-cd $(dirname $2)
+cd $(dirname $new_dir)
 export new_dir="`pwd -P`"
 
 cd $(dirname $0)
@@ -160,34 +183,16 @@ printf "done!\nNow counting contigs... "
 
 cd $new_dir
 
-num_contig=`grep -c "^>" $(basename $1)` #Just counts for the number of identifier lines.
+num_contig=`grep -c "^>" $(basename $file)` #Just counts for the number of identifier lines.
 
 printf "done!\n"
 
-echo There are $num_contig contigs in this file. Now splitting file and creating index.
-
-# We will use a maker script to split the genome file.
-# The new names of the files will come from the identifier.
-/isilon/biodiversity/pipelines/maker-2.10/maker-2.10/bin/fasta_tool --split $(basename $1)
+echo There are $num_contig contigs in this file.
 
 mkdir -p logs # Create a folder to store all the logs
 cd logs
 
-# As of this moment, there is no safe way to export arrays to a child process.
-# As a workaround, we will dump this array to a file and read it again to rebuild.
-IFS=$'\n'
-if [[ ! (-z $3 || $3 == '') ]]; then
-	export file_array=( $(grep "^>" ../$(basename $1) | cut -d"$3" -f 1 | cut -d'>' -f 2) )
-else
-	export file_array=( $(grep "^>" ../$(basename $1) | cut -f 1 | cut -d'>' -f 2) )
-fi
+#qsub -N "Maker_Parallel" -cwd -q all.q -p $priority -b y -v new_dir=$new_dir,filename=$filename,cpu=$cpu,from=$file,delimiter=$delimiter -V -pe smp $cpu -t 1-$num_contig:1 $script_path/$(basename $0)
 
-touch index
-
-for i in "${file_array[@]}"
-do
-	echo $i >> index
-done
-
-qsub -N "Maker_Parallel" -cwd -q all.q -p $priority -b y -v new_dir=$new_dir,filename=$filename,cpu=$cpu -V -pe smp $cpu -t 1-$num_contig:1 $script_path/$(basename $0)
+qsub -N "Maker_Parallel" -cwd -q all.q -p $priority -b y -v new_dir=$new_dir,filename=$filename,cpu=$cpu,from=$file,delimiter=$delimiter -V -pe smp $cpu -t 1-$num_contig:1 $script_path/$(basename $0)
 
