743be6b6e87b61e8d50d9277b01c4e4e93632e05
Jeff Cullis <jeffcullis@gmail.com>
2014-05-13 17:05:49 +0000

    JC: modified to allow running pipeline without necessarily subsetting based on contig length range
    git-svn-id: http://svn.biodiversity.agr.gc.ca/repo/source/Maker@5166 8f470391-0214-0410-be57-dbd57b47fce5

--- run_maker.sh
+++ run_maker.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 [-s <start contig size> -e <end contig size> -f <function>" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-w | -s <start contig size> -e <end contig size> -f <function>" 1>&2; exit 1; }
 
 cstart=
 cend=
 func=
 
-while getopts "s:e:f:" opt; do
+while getopts "ws:e:f:" opt; do
     case "${opt}" in
+        w)
+            contig_range="whole_genome"
         s)
             cstart=${OPTARG}
             ;;
@@ -22,13 +24,15 @@ done
 
 if [[ $cstart && $cend && $func ]]; then
     contig_range="$cstart-$cend"
+elif [[ $contig_range && $func ]]; then
+    x=
 else
     usage
 fi
 
 # export contig_range="x-y"
 genome_in=/isilon/biodiversity/projects/PRI_Se/assembly/clc-sendo.fasta
-genome_out=Se_PRI2_assembly.fa
+genome_raw=Se_PRI2_assembly.fa
 RNA_in=/isilon/biodiversity/users/cullisj/bug3124/CFIA_contigs.fa
 RNA=CFIA_RNA_contigs.fa
 proteins_in=/isilon/biodiversity/users/cullisj/bug3132/B_dendrobatidis/Batde5_best_proteins.fasta
@@ -42,9 +46,15 @@ genemark_es_dir=genemark_es
 genemark_sn_dir=genemark_sn
 gm_hmm=
 
-gensub=Se_PRI2_assembly_${contig_range}
-gensub_fa=$gensub.fa
-#maker_out=$maker1_dir/$gensub.maker.output
+genome="${genome_raw%.*}"
+genome_fa=$genome_raw
+if [[ contig_range != "whole_genome" ]]; then
+    genome=Se_PRI2_assembly_${contig_range}
+    genome_fa=$genome.fa
+    gensub=$genome
+    gensub_fa=$genome_fa
+fi
+#maker_out=$maker1_dir/$genome.maker.output
 #species=synchytrium_endobioticum
 augustus_species=synchytrium_endobioticum_${contig_range}
 
@@ -54,7 +64,7 @@ maker_opts=maker_opts.ctl
 
 dir_setup()
 {
-    ln -s $genome_in $genome_out
+    ln -s $genome_in $genome_raw
     ln -s $RNA_in $RNA
     ln -s $proteins_in $proteins
 }
@@ -62,7 +72,7 @@ dir_setup()
 sample_fasta()
 {
     [ -e ./fastaSizes.pl ] || svn export http://biodiversity/svn/source/misc_scripts/fastaSizes.pl
-    perl ./fastaSizes.pl -f $genome_out -r $contig_range -o $gensub_fa
+    perl ./fastaSizes.pl -f $genome_raw -r $contig_range -o $gensub_fa
 }
 
 check_organism()
@@ -83,10 +93,10 @@ run_maker1()
 {
     # Run first pass of maker
     mkdir $maker1_dir
-    cp $gensub_fa $RNA $proteins $maker1_dir/
+    cp $genome_fa $RNA $proteins $maker1_dir/
     cd $maker1_dir
     maker -CTL
-    replace_vars $maker_opts genome $gensub_fa
+    replace_vars $maker_opts genome $genome_fa
     replace_vars $maker_opts est $RNA
     replace_vars $maker_opts protein $proteins
     replace_vars $maker_opts est2genome 1
@@ -97,11 +107,11 @@ run_maker1()
     replace_vars $maker_exe RepeatMasker \\/isilon\\/biodiversity\\/pipelines\\/maker-2.10\\/RepeatMasker-open-4-0-3\\/RepeatMasker
     replace_vars $maker_exe exonerate \\/opt\\/bio\\/exonerate\\/bin\\/exonerate
     
-    maker_out=$gensub.maker.output
+    maker_out=$genome.maker.output
     /isilon/biodiversity/pipelines/maker-2.10/maker-2.10/bin/maker
     /isilon/biodiversity/pipelines/maker-2.10/maker-2.10/bin/gff3_merge -d $maker_out/*_master_datastore_index.log
     /isilon/biodiversity/pipelines/maker-2.10/maker-2.10/bin/fasta_merge -d $maker_out/*_master_datastore_index.log
-    sed '/FASTA/q' $gensub.all.gff | sed '$d' >$gensub.all.nofa.gff
+    sed '/FASTA/q' $genome.all.gff | sed '$d' >$genome.all.nofa.gff
     
     # Line below creates genome.ann, genome.dna files, required by augustus, snap, ..
     /isilon/biodiversity/pipelines/maker-2.10/maker-2.10/bin/maker2zff -d $maker_out/*_master_datastore_index.log
@@ -114,7 +124,8 @@ setup_augustus()
     # Only run this if you've never set up before
     mkdir ~/augustus
     cp -r /isilon/biodiversity/pipelines/maker-2.10/augustus.2.7/config ~/augustus/
-    echo "export AUGUSTUS_CONFIG_PATH=~/augustus/config" >> ~/.bashrc
+    echo "export AUGUSTUS_CONFIG_PATH=$HOME/augustus/config" >> ~/.bashrc
+    echo "export AUGUSTUS_CONFIG_PATH=$HOME/augustus/config" >> ~/.bash_profile
     . ~/.bashrc
 }
 
@@ -123,9 +134,9 @@ setup_augustus()
 train_augustus1()
 {
     mkdir -p augustus1
-    cp $maker1_dir/$gensub.all.nofa.gff $maker1_dir/genome.dna augustus1/
+    cp $maker1_dir/$genome.all.nofa.gff $maker1_dir/genome.dna augustus1/
     cd augustus1
-    /isilon/biodiversity/pipelines/maker-2.10/augustus.2.7/scripts/gff2gbSmallDNA.pl $gensub.all.nofa.gff genome.dna 1000 genes.gb
+    /isilon/biodiversity/pipelines/maker-2.10/augustus.2.7/scripts/gff2gbSmallDNA.pl $genome.all.nofa.gff genome.dna 1000 genes.gb
     #above breaks if fasta is in gff file.
     /isilon/biodiversity/pipelines/maker-2.10/augustus.2.7/scripts/new_species.pl --species=$augustus_species
 
@@ -158,7 +169,7 @@ train_snap()
 	cd params
 	/isilon/biodiversity/pipelines/maker-2.10/snap-2013-16/forge ../export.ann ../export.dna
 	cd ..
-	/isilon/biodiversity/pipelines/maker-2.10/snap-2013-16/hmm-assembler.pl $gensub params > $gensub.hmm
+	/isilon/biodiversity/pipelines/maker-2.10/snap-2013-16/hmm-assembler.pl $genome params > $genome.hmm
 	#Note: At the last step, you can replace my-genome with a name and my-genome.hmm
 	#with the file name.
 }
@@ -167,11 +178,11 @@ train_snap()
 train_genemark_es()
 {
     mkdir $genemark_es_dir
-    cp $gensub_fa $genemark_es_dir/
+    cp $genome_fa $genemark_es_dir/
     #cp $maker1_dir/genome.ann $maker1_dir/genome.dna $genemark_es_dir/
     cd $genemark_es_dir
     
-    /isilon/biodiversity/pipelines/maker-2.10/gene-mark-es-2.3e/gmes/gm_es.pl $gensub_fa
+    /isilon/biodiversity/pipelines/maker-2.10/gene-mark-es-2.3e/gmes/gm_es.pl $genome_fa
 	# Note: If GeneMark fails, there might be something wrong with your genome.
 	# If your contigs are short, try adding --min_contig 10,000 and --max_nnn 5000
     # When completed, the training file is ./mod/es.mod. Note that this is a symlink
@@ -183,32 +194,32 @@ train_genemark_es()
 train_genemark_sn()
 {
     mkdir $genemark_sn_dir
-    cp $gensub_fa $genemark_sn_dir/
+    cp $genome_fa $genemark_sn_dir/
     cd $genemark_sn_dir
     name=gmsn
-    gmsn.pl --combine --species $augustus_species -gm --name $name $gensub_fa
+    gmsn.pl --combine --species $augustus_species -gm --name $name $genome_fa
     # When completed, the training file is <name>_hmm_combined.mod.
 }
 
 run_maker2()
 {
     mkdir $maker2_dir
-    cp $gensub_fa $RNA $proteins $maker2_dir
+    cp $genome_fa $RNA $proteins $maker2_dir
     # cp $snap_dir/path/to/snaphmm snap.hmm
     # if [ type=eukaryote ]; then ...
     cp $gm_hmm $maker2_dir/gm_es.mod $maker2_dir/ # ln -s to be able to see target here?
     
-    cp $maker1_dir/$gensub.all.gff $maker2_dir
+    cp $maker1_dir/$genome.all.gff $maker2_dir
     
     cp $maker1_dir/*ctl $maker2_dir/
     cd $maker2_dir
     #maker -CTL
     
-    replace_vars $maker_opts genome $gensub_fa
+    replace_vars $maker_opts genome $genome_fa
     replace_vars $maker_opts est $RNA
     replace_vars $maker_opts protein $proteins
     # Above three commands as in run_maker1
-    replace_vars $maker_opts genome_gff  $gensub.all.gff
+    replace_vars $maker_opts genome_gff  $genome.all.gff
     
     replace_vars $maker_opts snaphmm snap.hmm
     replace_vars $maker_opts gmhmm gm_es.mod
